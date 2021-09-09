de2483c05a004b8c02f426c1b57bfd0ca1d8bbdf
Sarah <sarahdelfino7@gmail.com>
2021-09-09 00:01:59 -0400

    creating initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
